2c586fe9e074c71b74d85a2b7df50fb3d6526944
68115502 <68115502@L815WD220005.edu.cbe.ab.ca>
2026-04-27 14:03:47 -0600

    2:03PM

--- Chapter8/src/Mastery/University.java
+++ Chapter8/src/Mastery/University.java
@@ -16,13 +16,13 @@ public class University
     public static void main(String[] args) 
     {
     	 // Create Faculty and Staff objects with name, salary, and extra attributes
-        Faculty f1 = new Faculty("Dua Fatima", 60000, "Computer Science");
+        Faculty f1 = new Faculty("Dua Fatima", 0, "Computer Science");
         Staff s1 = new Staff("Abdulkadir Abdalla", 90000, "Teacher");
         
-        Faculty f2 = new Faculty("Usman Elmi", 20000, "Fine Arts");
+        Faculty f2 = new Faculty("Usman Elmi", 0, "Fine Arts");
         Staff s2 = new Staff("I. Burgess", 50000, "Teacher");
         
-        Faculty f3 = new Faculty("Eman Abid", 80000, "Chemsitry");
+        Faculty f3 = new Faculty("Eman Abid", 0, "Chemsitry");
         Staff s3 = new Staff("Rebecca Jewers", 100000, "Teacher");
 
         // Print details of first faculty and staff
@@ -42,3 +42,34 @@ public class University
         System.out.println(s3);
     }
 }
+
+/*
+
+Name: Dua Fatima
+Salary: $0.0
+Department: Computer Science
+
+
+Name: Abdulkadir Abdalla
+Salary: $90000.0
+Job Title: Teacher
+
+Name: Usman Elmi
+Salary: $0.0
+Department: Fine Arts
+
+
+Name: I. Burgess
+Salary: $50000.0
+Job Title: Teacher
+
+Name: Eman Abid
+Salary: $0.0
+Department: Chemsitry
+
+
+Name: Rebecca Jewers
+Salary: $100000.0
+Job Title: Teacher
+
+*/
